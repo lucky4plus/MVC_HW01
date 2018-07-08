3f3c53787bc46607a606d856ca5a9817fb6180dc
lucky4plus <33918431+lucky4plus@users.noreply.github.com>
2018-07-09 01:10:10 +0800

    1.依切版規劃layout 2.建立ViewModel 3.透過Child Action產生50筆列表

--- Money/Money/Controllers/HomeController.cs
+++ Money/Money/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Money.Models.ViewModels;
 namespace Money.Controllers
 {
     public class HomeController : Controller
@@ -12,7 +12,19 @@ namespace Money.Controllers
         {
             return View();
         }
-
+        [ChildActionOnly]
+        public ActionResult ListMyMoney()
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            DateTime today = DateTime.Now;
+            var result = new MoneyViewModel
+            {
+                Category = rnd.Next(1, 3),
+                Money = rnd.Next(1, 10000),
+                Date = today.AddDays(rnd.Next(0, 50))
+            };
+            return View(result);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ Money/Money/Models/ViewModels/MoneyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Money.Models.ViewModels
+{
+    public class MoneyViewModel
+    {
+        public int Category { get; set; }
+        public decimal Money { get; set; }
+        public DateTime Date { get; set; }
+        public string Remark { get; set; }
+    }
+    public enum MyCategory { 支出 = 1, 收入 };
+}

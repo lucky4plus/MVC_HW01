3a52ddb2217c1aab74010aaad26dd5e4ce8958aa
lucky4plus <33918431+lucky4plus@users.noreply.github.com>
2018-07-20 01:13:38 +0800

    加入資料庫檔案與建立model，使用code first

---
+++ Money/Money/Models/MoneyModel.cs
@@ -0,0 +1,21 @@
+namespace Money.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class MoneyModel : DbContext
+    {
+        public MoneyModel()
+            : base("name=MoneyModel")
+        {
+        }
+
+        public virtual DbSet<AccountBook> AccountBook { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
--- Money/Money/Models/ViewModels/MoneyViewModel.cs
+++ Money/Money/Models/ViewModels/MoneyViewModel.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+
 namespace Money.Models.ViewModels
 {
     public class MoneyViewModel
     {
+        [Required]
+        [DisplayName("類別")]
         public int Category { get; set; }
-        public decimal Money { get; set; }
+        [Required]
+        [DisplayName("金額")]
+        [Range(0, int.MaxValue)]
+        public int Money { get; set; }
+        [Required]
+        [DisplayName("日期")]
         public DateTime Date { get; set; }
+        [Required]
+        [DisplayName("備註")]
+        [MaxLength(100, ErrorMessage = "長度不可超過100字")]
         public string Remark { get; set; }
     }
     public enum MyCategory { 支出 = 1, 收入 };

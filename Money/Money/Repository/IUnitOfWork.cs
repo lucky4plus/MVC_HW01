25c7807c149af1f022b1c63425da3de26f29c336
lucky4plus <33918431+lucky4plus@users.noreply.github.com>
2018-07-20 01:10:31 +0800

    使用來自資料庫的code first 建立model Migrations（資料庫遷移） 加入Repository 與UnitOfWork

---
+++ Money/Money/Repository/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+
+namespace Money.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// context
+        /// </summary>
+        DbContext Context { get; set; }
+        /// <summary>
+        /// save change
+        /// </summary>
+        void Commit();
+    }
+}
---
+++ Money/Money/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Money.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Money.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; set; }
+
+        public UnitOfWork()
+        {
+            Context = new MoneyModel();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
